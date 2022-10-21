b26a58b97c728d2ad1e70d2bad21e045920efd30
tbinkasem <tbinkasem@gmail.com>
2022-10-21 11:50:44 +0700

    ดีใจจัง

---
+++ comment.php
@@ -0,0 +1,15 @@
+<?php
+
+    //บรรทัดที่ 6-7 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+    echo "Teera Binkasem<br>";
+    echo "Information Technology";
+    //echo "Pattani<br>";
+    #echo "Thailand";
+
+     //บรรทัดที่ 10-12 แสดงผลการใช้งาน Comment แบบหลายบรรทัด
+    /* echo "<br>";
+    echo "94000<br>";
+    echo "Asia<br>"; */
+    echo "Universal";
+
+?>
